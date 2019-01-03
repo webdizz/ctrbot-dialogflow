fd9feb1c24134a32558c05098cc67bccfe6562d9
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-03 19:41:46 +0200

    Add types and working simple TS code with DialogFlow Fulfilment

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from 'express'
+import { WebhookClient } from 'dialogflow-fulfillment'
+import { createLogger } from 'bunyan'
 
 export function webhookHandler(req: Request, res: Response) {
-    console.log(req)
 
+    const LOG = createLogger({ name: 'ctrbot/index' })
+    LOG.info(req)
+    LOG.info(process.env.DIALOGFLOW_AUTH_TOKEN)
+
+    let agent = new WebhookClient({ request: req, response: res })
+    LOG.info(agent.intent, agent.contexts, agent.parameters, agent.session)
     res.status(200).type("application/json").send('{"result":"OK"}')
 }
