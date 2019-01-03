4ae3d259a24130ccce34870e810ea4a41ba64d30
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-03 19:42:06 +0200

    Add local runner for function

---
+++ index.local.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    webhookRouter = express.Router(),
+    cors = require("cors"),
+    path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+const webhook = require('./build/index')
+
+webhookRouter.post('*', function (req, res) {
+    webhook.webhookHandler(req, res)
+});
+
+app.use('/webhookHandler', webhookRouter);
+
+app.listen(1337, function () {
+    console.log('Webhook is  listening...');
+});
