395dae9a32dd7ece393a9e046f1484e0549d15f1
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-03 19:53:03 +0200

    Add request eligibility validation

--- index.ts
+++ index.ts
@@ -2,13 +2,19 @@ import { Request, Response } from 'express'
 import { WebhookClient } from 'dialogflow-fulfillment'
 import { createLogger } from 'bunyan'
 
+const LOG = createLogger({ name: 'ctrbot/index' })
+
 export function webhookHandler(req: Request, res: Response) {
+    if (process.env['DIALOGFLOW_AUTH_TOKEN'] == req.header('DIALOGFLOW_AUTH_TOKEN')) {
+        let result = handleWebhookRequest(req, res)
+        res.status(200).type("application/json").send(result)
+    } else {
+        res.status(404).send('No such page.')
+    }
+}
 
-    const LOG = createLogger({ name: 'ctrbot/index' })
-    LOG.info(req)
-    LOG.info(process.env.DIALOGFLOW_AUTH_TOKEN)
-
-    let agent = new WebhookClient({ request: req, response: res })
-    LOG.info(agent.intent, agent.contexts, agent.parameters, agent.session)
-    res.status(200).type("application/json").send('{"result":"OK"}')
+function handleWebhookRequest(request: Request, response: Response) {
+    let agent = new WebhookClient({ request: request, response: response })
+    LOG.info({ intent: agent.intent, contexts: agent.contexts, parameters: agent.parameters, session: agent.session })
+    return '{"result":"OK"}'
 }
