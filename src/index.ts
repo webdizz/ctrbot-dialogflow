db9b59c89730b9a4dcc488ea1979e71be1c1f3c5
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-03 20:37:52 +0200

    Implement the first intent that can be fulfilled by GCP clooud function

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,7 @@ const LOG = createLogger({
 
 export function webhookHandler(req: Request, res: Response) {
     if (process.env['DIALOGFLOW_AUTH_TOKEN'] == req.header('DIALOGFLOW_AUTH_TOKEN')) {
-        let result = handleWebhookRequest(req, res)
-        res.status(200).type("application/json").send(result)
+        handleWebhookRequest(req, res)
     } else {
         res.status(404).send('No such page.')
     }
@@ -25,5 +24,14 @@ export function webhookHandler(req: Request, res: Response) {
 function handleWebhookRequest(request: Request, response: Response) {
     let agent = new WebhookClient({ request: request, response: response })
     LOG.debug({ intent: agent.intent, contexts: agent.contexts, parameters: agent.parameters, session: agent.session })
-    return '{"result":"OK"}'
+
+    let intentMap = new Map();
+    intentMap.set('order-delivery-tracking_order-number', handleOrderTracking);
+
+    agent.handleRequest(intentMap);
+}
+
+function handleOrderTracking(agent: WebhookClient) {
+    let orderNumber = agent.parameters['order-number']
+    agent.add(`Hello, your order ${orderNumber} will arrive....`)
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,6 @@
 declare module 'dialogflow-fulfillment'
+
+declare class WebhookClient {
+    parameters: string[]
+    add(msg: string): void
+}
