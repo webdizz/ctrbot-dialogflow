59173c179cae854220f3421ffb1945acb23aac85
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-04 13:36:36 +0200

    Replace plain bunyan with @restify-ts/logger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express'
 import { WebhookClient } from 'dialogflow-fulfillment'
-import { createLogger, levelFromName, LogLevelString } from 'bunyan'
 
-let logLevel = process.env['LOGGING_LEVEL'] as LogLevelString || levelFromName.info
+import { Logger } from '@restify-ts/logger';
 
-const LOG = createLogger({
+let LOG = new Logger({
     name: 'ctrbot/index',
-    stream: process.stdout,
-    level: logLevel
-})
+    level: process.env['LOGGING_LEVEL'] || 'info',
+    streams: [
+        {
+            stream: process.stdout,
+            level: 'trace'
+        }
+    ]
+});
 
 export function webhookHandler(req: Request, res: Response) {
     if (process.env['DIALOGFLOW_AUTH_TOKEN'] == req.header('DIALOGFLOW_AUTH_TOKEN')) {
@@ -25,12 +29,14 @@ export function webhookHandler(req: Request, res: Response) {
  */
 function handleWebhookRequest(request: Request, response: Response) {
     let agent = new WebhookClient({ request: request, response: response })
-    LOG.debug({ intent: agent.intent, contexts: agent.contexts, parameters: agent.parameters, session: agent.session })
+    LOG.error({ intent: agent.intent, contexts: agent.contexts, parameters: agent.parameters, session: agent.session }, 'error message')
+    LOG.info({ intent: agent.intent, contexts: agent.contexts, parameters: agent.parameters, session: agent.session }, 'info message')
+    LOG.debug({ intent: agent.intent, contexts: agent.contexts, parameters: agent.parameters, session: agent.session }, 'debug message')
 
-    let intentMap = new Map();
-    intentMap.set('order-delivery-tracking_order-number', handleOrderTracking);
+    let intentMap = new Map()
+    intentMap.set('order-delivery-tracking_order-number', handleOrderTracking)
 
-    agent.handleRequest(intentMap);
+    agent.handleRequest(intentMap)
 }
 
 function handleOrderTracking(agent: WebhookClient) {
