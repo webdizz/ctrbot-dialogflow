b6e10ff47b883e5f74e0fd3d3cca41dd22e73dd2
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-04 18:10:10 +0200

    Add async await proper support

--- src/intents/order-tracking.ts
+++ src/intents/order-tracking.ts
@@ -1,27 +1,50 @@
 import * as request from 'request-promise-native'
+import { Logger } from '@restify-ts/logger';
 
-class OrderTrackingIntentHandler {
-    constructor() { }
+export class OrderTrackingIntentHandler {
 
-    async handleOrderTracking(agent: WebhookClient) {
-        let intentContextParams = agent.contexts[0].parameters
-        let orderNumber = intentContextParams['order-number']
-        let email = intentContextParams['order-email']
+    constructor(private agent: WebhookClient, private log: Logger) { }
 
-        const ENDPOINT_ORDER_DETAILS = process.env['ENDPOINT_ORDER_DETAILS'] as string
-        let options = {
-            uri: ENDPOINT_ORDER_DETAILS,
-            body: {
-                orderId: orderNumber,
-                email: email
-            },
-            json: true
+    /**
+     *  Handles order tracking intent
+     */
+    public handleOrderTracking() {
+        return async () => {
+            let intentContextParams = this.agent.contexts[0].parameters
+            let orderNumber = intentContextParams['order-number']
+            let email = intentContextParams['order-email']
+
+            const ENDPOINT_ORDER_DETAILS = process.env['ENDPOINT_ORDER_DETAILS'] as string
+            let options = {
+                uri: ENDPOINT_ORDER_DETAILS,
+                body: {
+                    orderId: orderNumber,
+                    email: email
+                },
+                json: true
+            }
+            await request.post(options)
+                .then((response) => {
+                    this.handleOrderTrackingResponse(response, orderNumber)
+                }).catch(error => {
+                    this.log.error(error)
+                    this.agent.add(`I'm really sorry, but something wrong happened when I tried to find your order '${orderNumber}' ;(`)
+                })
         }
-        const response = await request.post(options)
-
-        agent.add(`Hello, your order '${orderNumber}' status is '${response.status}' and will arrive at ${response.products['order.status.inprogress'][0].eta}`)
     }
 
-}
+    private handleOrderTrackingResponse(response: any, orderNumber: string) {
+        let eta = this.formatEta(response.products['order.status.inprogress'][0].eta);
+        this.agent.add(`Hello, your order '${orderNumber}' status is '${response.status}' and will arrive on ${eta}`)
+    }
 
-export { OrderTrackingIntentHandler }
+    private formatEta(etaOrigin: string) {
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            weekday: 'short'
+        }
+        return new Intl.DateTimeFormat('en-US', options).format(Date.parse(etaOrigin))
+    }
+}
